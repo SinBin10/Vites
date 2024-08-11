c3747d1473f251c131514d29035c0f09e4849bf7
SinBin10 <bincode10@gmail.com>
2024-08-11 18:28:22 +0530

    learned about debug library for logging errors

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
+//debugger used for printing errors during development instead of console.log()
+// they are only printed when environement variable is set
+// development is the namespace
+const dbgr = require("debug")("development: mongoose");
 
 //might give error some times so we have to do error handling
 mongoose
   .connect("mongodb://127.0.0.1:27017/vitesdb")
   .then(function () {
-    console.log("connected");
+    dbgr("connected");
   })
   .catch(function (err) {
-    console.log(err);
+    dbgr(err);
   });
 
 module.exports = mongoose.connection;
