83ace8fff9d5742fe3ab47fbed3d863516995264
SinBin10 <bincode10@gmail.com>
2024-08-11 13:25:40 +0530

    creating separating config file for mongodb connection.

---
+++ config/mongoose-connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+//might give error some times so we have to do error handling
+mongoose
+  .connect("mongodb://127.0.0.1.27017/vitesdb")
+  .then(function () {
+    console.log("connected");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+module.exports = mongoose.connection;
