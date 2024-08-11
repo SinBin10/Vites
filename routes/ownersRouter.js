07a2a51bb141d0360f107a9602d6d293d242bf87
SinBin10 <bincode10@gmail.com>
2024-08-11 23:05:01 +0530

    making selected routes work only during development.

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+//even if we forget to remove this code still this code wont be uploaded to production
+if (process.env.NODE_ENV === "development") {
+  console.log("working...");
+  router.post("/c", (req, res) => {
+    res.send("its working absolutely fine !!");
+  });
+}
+
 router.get("/", (req, res) => {
   res.send("its working absolutely fine !!");
 });
